b2ac10a5bff43f4f16b904cefdbf3fef8a20e705
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-11-06 14:37:13 +0900

    Mac: インストールスクリプト仮作成

---
+++ Mac/README.md
@@ -0,0 +1,14 @@
+## Mac
+
+### 各種インストール
+※この dotfiles リポジトリをローカルに落としてきた上で実行すること
+
+XCode の command line tool をいれる
+```bash
+$ xcode-select install
+```
+
+スクリプト実行例
+```bash
+$ bin/zsh ./Mac/bundle/install.sh
+```
---
+++ Mac/bundle/install.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+set -eux
+EXEPATH=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+DOTFILESRAW = "https://raw.githubusercontent.com/h-yoshikawa44/dotfiles/main"
+
+## ========== brew ==========
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+brew upgrade
+brew bundle --file "${EXEPATH}"/Brewfile
+
+## ========== settings ==========
+curl "${$DOTFILESRAW}/Mac/.zprofile" -o "${HOME}/.zprofile"
