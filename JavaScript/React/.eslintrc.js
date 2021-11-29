1aba5eb508b905da72c02ec46ac2bd3afeb0cdfc
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-11-29 13:39:43 +0900

    JS: ESLint設定を更新

--- JavaScript/React/.eslintrc.js
+++ JavaScript/React/.eslintrc.js
@@ -1,9 +1,17 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'plugin:react/recommended',
     'airbnb',
     'prettier'
   ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx']}]
   }
