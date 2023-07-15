17f6f8b504ea1ae7db967410ac29129a2085736f
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2023-07-15 18:06:26 +0900

    Astro: UnoCSS設定ファイルの例を更新

--- TypeScript/Astro/uno.config.ts
+++ TypeScript/Astro/uno.config.ts
@@ -1,5 +1,52 @@
-import { defineConfig } from 'unocss'
+import {
+  defineConfig,
+  presetUno,
+  presetWebFonts,
+  presetIcons,
+  transformerDirectives,
+} from 'unocss';
 
 export default defineConfig({
-  // ...UnoCSS options
-})
+  presets: [
+    presetUno(),
+    presetWebFonts({
+      fonts: {
+        sawarabi: [
+          'Sawarabi Mincho',
+          'Verdana',
+          { name: 'sans-serif', provider: 'none' },
+        ],
+        fatface: ['Abril Fatface', { name: 'sans-serif', provider: 'none' }],
+      },
+    }),
+    presetIcons(),
+  ],
+  transformers: [transformerDirectives()],
+  theme: {
+    fontFamily: {
+      hackGen: [
+        'HackGen',
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Liberation Mono',
+        'Courier New',
+        'monospace',
+      ],
+    },
+    colors: {
+      brand: {
+        primary: '#202020',
+        start: '#323232',
+        end: '#0e0e0e',
+      },
+    },
+  },
+  // スタイル生成を強制したいクラスがあれば定義
+  safelist: '',
+  shortcuts: {
+    // 複雑なスタイルの組み合わせ定義を使いまわすとき
+  },
+});
