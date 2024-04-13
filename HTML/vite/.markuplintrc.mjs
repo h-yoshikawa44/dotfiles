5a202db2ee8eba272afbb3e65d72bce2a5b9ba9b
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-04-13 16:55:58 +0900

    fix: markuplintrcの型指定がずれたので正しいものに修正

--- HTML/vite/.markuplintrc.mjs
+++ HTML/vite/.markuplintrc.mjs
@@ -1,4 +1,4 @@
-/** @type {import("markuplint").Config} */
+/** @type {import("@markuplint/ml-config").Config} */
 export default {
   extends: ['markuplint:recommended-static-html'],
   excludeFiles: ['./node_modules/**/*.html'],
