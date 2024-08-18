4881e8f813bda59c6eedc99059aec803b68ec393
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2024-08-18 17:39:06 +0900

    HTML: 素のJS時はNode.js特化のESLintルールが不要なので外した

--- HTML/vite/js/eslint.config.js
+++ HTML/vite/js/eslint.config.js
@@ -1,4 +1,3 @@
-import js from '@eslint/js';
 import globals from 'globals';
 import neostandard, { plugins } from 'neostandard';
 
@@ -7,12 +6,10 @@ export default [
   {
     ignores: ['node_modules/**', 'eslint.config.js'],
   },
-  js.configs.recommended,
   ...neostandard(),
   plugins['@stylistic'].configs.customize({
     semi: true,
   }),
-  plugins.n.configs['flat/recommended-module'],
   plugins.promise.configs['flat/recommended'],
   {
     files: ['js/**/*.js'],
